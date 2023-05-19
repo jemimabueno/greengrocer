908a8dd9c9af3070ef693edf1660baaaf69af459
Jemima Bueno <jemimabueno@hotmail.com>
2023-05-19 15:53:00 -0300

    tela de cadastro

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -3,43 +3,106 @@ import 'package:greengrocer/src/auth/components/custom_text_field.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
 class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({super.key});
+  SignUpScreen({super.key});
+
+  final cpfFormatter = ();
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
-      body: Column(
-        children: [
-        Expanded(
-          child: Center(
-            child: Text('Cadastro',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize:35,),),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        'Cadastro',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35,
+                        ),
+                      ),
+                    ),
+                  ),
+                  //Formulário
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 32, vertical: 40),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(45),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const CustomTextField(
+                            icon: Icons.email, label: 'Email'),
+                        const CustomTextField(
+                          icon: Icons.lock,
+                          label: 'Senha',
+                          isSecret: true,
+                        ),
+                        const CustomTextField(
+                          icon: Icons.person,
+                          label: 'Nome',
+                        ),
+                        const CustomTextField(
+                          icon: Icons.phone,
+                          label: 'Celular',
+                        ),
+                        const CustomTextField(
+                          icon: Icons.file_copy,
+                          label: 'CPF',
+                        ),
+                        SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18))),
+                            onPressed: () {},
+                            child: const Text(
+                              'Cadastrar Usuário',
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                top: 10,
+                left: 10,
+                child: SafeArea(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        //Formulário
-        Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 32,
-          vertical: 40),
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(45),
-            )
-          ),
-          child: Column(children: [ 
-            CustomTextField(icon: Icons.email, label: 'email'),      
-            CustomTextField(icon: Icons.lock, label: 'Senha'),
-            CustomTextField(icon: Icons.person, label: 'Nome'),
-            CustomTextField(icon: Icons.phone, label: 'Celular'),
-            CustomTextField(icon: Icons.file_copy, label: 'CPF'),
-          ],),
-        ),
-
-      ],),
+      ),
     );
   }
 }
