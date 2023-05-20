61152794d48fb8eaabe68825634feaac361e7eef
Jemima Bueno <jemimabueno@hotmail.com>
2023-05-20 14:34:00 -0300

    configurando quantidade de numeros cpf

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class SignUpScreen extends StatelessWidget {
   SignUpScreen({super.key});
 
-  final cpfFormatter = ();
+  final cpfFormatter = MaskTextInputFormatter (
+    mask: '###.###.###-##',
+    filter: {'#':RegExp(r'[0-9]')},
+  );
+
+  final phoneFormatter = MaskTextInputFormatter (
+    mask: '## # ####-####',
+    filter: {'#':RegExp(r'[0-9]')},
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -56,13 +65,15 @@ class SignUpScreen extends StatelessWidget {
                           icon: Icons.person,
                           label: 'Nome',
                         ),
-                        const CustomTextField(
+                        CustomTextField(
                           icon: Icons.phone,
                           label: 'Celular',
+                          inputFormatters: [phoneFormatter],
                         ),
-                        const CustomTextField(
+                         CustomTextField(
                           icon: Icons.file_copy,
                           label: 'CPF',
+                          inputFormatters: [cpfFormatter],
                         ),
                         SizedBox(
                           height: 50,
