4bfc358e2d592c823dbd9e5d7a8baf8d01ef97df
Jemima Bueno <jemimabueno@hotmail.com>
2023-05-20 14:31:08 -0300

    botoes tela base

---
+++ lib/src/base/base_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatelessWidget {
+  const BaseScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(color: Colors.white),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.green,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white.withAlpha(100),
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined), label: 'Home'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_outlined),
+              label: 'Carrinho',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'Pedidos',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_2_outlined), label: 'Perfil')
+          ],
+        ));
+  }
+}
