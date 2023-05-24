4da8d60f2182adf351f854a5ba37a4f59dd7d754
Jemima Bueno <jemimabueno@hotmail.com>
2023-05-24 09:55:37 -0300

    .

--- lib/src/config/app_data.dart
+++ lib/src/config/app_data.dart
@@ -2,7 +2,7 @@ import 'package:greengrocer/src/models/item_model.dart';
 
 ItemModel apple = ItemModel(
   description: 'A melhor maçã da região',
-  imgUrl: 'assets\fruits\apple.png',
+  imgUrl:'assets/fruits/apple.png',
   itemName: 'Maçã',
   price: 5.5,
   unit: 'kg',
